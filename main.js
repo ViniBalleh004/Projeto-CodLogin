bbd907a5c4146c2c348002c5aa94ce2bed8b033c
ViniBalleh004 <ballesterobranco224@gmail.com>
2025-04-15 21:58:53 -0300

    função de excluir e editar adicionadas ao código

--- main.js
+++ main.js
@@ -21,6 +21,7 @@ function salvarUser(){
     if(nomeUser){
         dadosLista.push(nomeUser);
         console.log(dadosLista);
+        criaLista();
         document.getElementById('nomeUser').valeu = "";
     } else{
         alert("Favor informar um nome para o cadastro");
@@ -28,9 +29,29 @@ function salvarUser(){
 }
 
 //Função para preencher a lista de cadastro
-function criaLista(){
+function criaLista() {
     let tabela = "<tr><th>Nome</th><th>Ações</th></tr>";
-    for(let i = 0; i <= (dadosLista.length-1); i++){
-        tabela += "<tr><th>"+ dadosLista[i]  +"</th><th>Ações</th></tr>"
-    }    
+    for (let i = 0; i <= (dadosLista.length - 1); i++) {
+      tabela += `
+      <tr>
+        <td>${dadosLista[i]}</td>
+        <td>
+          <button class='btn btn-warning' onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button>
+          <button class='btn btn-danger' onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button>
+        </td>
+      </tr>`;
+      document.getElementById('tabela').innerHTML = tabela;
+    }
+  }
+
+//Função para excluir o nome da lista
+function excluir(i){
+    dadosLista.splice((i-1), 1);
+    document.getElementById('tabela').deleteRow(i);
+}
+
+// Função para edição de nome da lista
+function editar(i){
+    document.getElementById('nomeUser').value = dadosLista[(i - 1)];
+    dadosLista.splice(dadosLista[(i - 1)], 1);
 }
